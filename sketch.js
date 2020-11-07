737ed8c2ed7289d08b6a8afe4d86476c3325ea8b
eungdin98 <73861583+eungdin98@users.noreply.github.com>
2020-11-07 21:21:40 +0900

    Following bug
    마우스를 따라다니는 벌레

--- sketch.js
+++ sketch.js
@@ -1,84 +1,63 @@
-let movers = [];
-let rad = 60;
-let xpos, ypos;
 
-let xspeed = 3.8;
-let yspeed = 3.2;
 
-let xdirection = 1;
-let ydirection = 1;
 
-let G = 1;
+let s1, s2;
+let gravity = 9.0;
+let mass = 10.0;
 
 function setup() {
-  createCanvas(700, 700);
-  frameRate(30);
-  ellipseMode(RADIUS);
-  xpos = width / 2;
-  ypos = height / 2;
-
-  for (let i = 0; i < 25; i++) {
-    movers[i] = new Mover(random(width), random(height), random(0.1, 2));
-  }
+  createCanvas(720, 400);
+  fill(255, 126);
+  // 입력: x, y, mass(질량), gravity(중력)
+  s1 = new Spring2D(0.0, width / 2, mass, gravity);
+  s2 = new Spring2D(0.0, width / 2, mass, gravity);
+  s3 = new Spring2D(0.0, width / 2, mass, gravity);
+  s4 = new Spring2D(0.0, width / 2, mass, gravity);
+  s5 = new Spring2D(0.0, width / 2, mass, gravity);
 }
 
 function draw() {
-  background(50, 89, 100);
-  
-  xpos = xpos + xspeed * xdirection;
-  ypos = ypos + yspeed * ydirection;
+  background(0);
+  s1.update(mouseX, mouseY);
+  s1.display(mouseX, mouseY);
+  s2.update(s1.x, s1.y);
+  s2.display(s1.x, s1.y);
+  s3.update(s2.x, s2.y);
+  s3.display(s2.x, s2.y);
+  s4.update(s3.x, s3.y);
+  s4.display(s3.x, s3.y);
+  s5.update(s4.x, s4.y);
+  s5.display(s4.x, s4.y);
+}
 
-   if (xpos > width - rad || xpos < rad) {
-    xdirection *= -1;
-  }
-  if (ypos > height - rad || ypos < rad) {
-    ydirection *= -1;
-  }
-  
-  for (let i = 0; i < movers.length; i++) {
-    for (let j = 0; j < movers.length; j++) {
-      if (i !== j) {
-        let force = movers[j].attract(movers[i]);
-        movers[i].applyForce(force);
-      }
-    }
-    ellipse(xpos, ypos, rad, rad);
-    movers[i].update();
-    movers[i].display();
+function Spring2D(xpos, ypos, m, g) {
+  this.x = xpos;// x 와 y 좌표
+  this.y = ypos;
+  this.vx = 0; // x축과 y축 속도
+  this.vy = 0;
+  this.mass = m;
+  this.gravity = g;
+  this.radius = 30;
+  this.stiffness = 0.2;
+  this.damping = 0.7;
+
+  this.update = function(targetX, targetY) {
+    let forceX = (targetX - this.x) * this.stiffness;
+    let ax = forceX / this.mass;
+    this.vx = this.damping * (this.vx + ax);
+    this.x += this.vx;
+    let forceY = (targetY - this.y) * this.stiffness;
+    forceY += this.gravity;
+    let ay = forceY / this.mass;
+    this.vy = this.damping * (this.vy + ay);
+    this.y += this.vy;
+}
+
+  this.display = function(nx, ny) {
+    noStroke();
+    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    stroke(255);
+    line(this.x, this.y, nx, ny);
   }
 }
-class Mover {
-  constructor(x, y, m) {
-    this.mass = m;
-    this.position = createVector(x, y);
-    this.velocity = createVector(0, 0);
 
-    this.acceleration = createVector(0, 0);
-  }
-
-  applyForce(force) {
-    let f = p5.Vector.div(force, this.mass);
-    this.acceleration.add(f);
-  }
-
-  update() {
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.acceleration.mult(0);
-  }
-
-  display() {
-    stroke(0);
-    strokeWeight(2);
-    fill(255, 127);
-    ellipse(this.position.x, this.position.y, this.mass * 16);
-  }
-  attract(other) {
-    let force = p5.Vector.sub(this.position, other.position);
-    let distance = force.mag();
-    distance = constrain(distance, 1.0, 5.0);
-    let strength = (G * this.mass * other.mass) / (distance * distance);
-    force.setMag(strength);
-    return force;
-  }
-}
